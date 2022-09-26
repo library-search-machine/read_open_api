70f7e797b61cf1f7a4ddc0c73bfb18f2c12c0646
Lee1231234 <qscgyd1629@naver.com>
2022-09-26 18:15:39 +0900

    엑셀로 변경 (이유: 데이터의 손실)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,23 @@ package com.example.demo;
 
 import com.example.demo.domain.library;
 import com.example.demo.repository.libraryRepository;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.FileInputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -24,6 +31,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
     }
+
     @Bean
     public CommandLineRunner demo(libraryRepository libraryRepository) {
 
@@ -55,10 +63,10 @@ public class DemoApplication {
                 page++;
             }while(resultInt>total);
 
-*/
 
+//csv 파일로
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("C:\\Users\\user\\Downloads\\도서관"))) {
-                for (Path file: stream) {
+                for (Path file : stream) {
                     List<List<String>> csvList = new ArrayList<>();
                     List<library> libraries = new ArrayList<>();
 
@@ -66,18 +74,18 @@ public class DemoApplication {
                     BufferedReader br = null;
                     String line = "";
 
-                    FileInputStream input=new FileInputStream("C:\\Users\\user\\Downloads\\도서관\\"+file.getFileName());
-                    InputStreamReader reader=new InputStreamReader(input,"UTF-8");
+                    FileInputStream input = new FileInputStream("C:\\Users\\user\\Downloads\\도서관\\" + file.getFileName());
+                    InputStreamReader reader = new InputStreamReader(input, "UTF-8");
                     try {
                         br = new BufferedReader(reader);
 
                         while ((line = br.readLine()) != null) { // readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
                             List<String> aLine;
                             String[] lineArr = line.split("\\|"); // 파일의 한 줄을 |로 나누어 배열에 저장 후 리스트로 변환한다.
-                            if(lineArr.length==0) continue;
+                            if (lineArr.length == 0) continue;
 
                             //한줄 밀리는 현상에 대해서 픽스하기위한 코드
-                            if(lineArr.length!=13){
+                            if (lineArr.length != 13) {
                                 StringBuilder sb = new StringBuilder();
                                 sb.append(line);
                                 line = br.readLine();
@@ -87,7 +95,7 @@ public class DemoApplication {
 
                             aLine = Arrays.asList(lineArr);
 
-                            libraries.add(new library(aLine.get(1),tmp[0],aLine.get(3),aLine.get(4),aLine.get(2),aLine.get(9),aLine.get(5),aLine.get(8)));
+                            libraries.add(new library(aLine.get(1), tmp[0], aLine.get(3), aLine.get(4), aLine.get(2), aLine.get(9), aLine.get(5), aLine.get(8)));
                             csvList.add(aLine);
 
                         }
@@ -101,7 +109,7 @@ public class DemoApplication {
                                 br.close(); // 사용 후 BufferedReader를 닫아준다.
 
                             }
-                        } catch(IOException e) {
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
@@ -111,13 +119,90 @@ public class DemoApplication {
             } catch (IOException | DirectoryIteratorException ex) {
                 System.err.println(ex);
             }
+*/
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("C:\\Users\\user\\Downloads\\도서관 엑셀"))) {
+                for (Path file : stream) {
+                    List<List<String>> csvList = new ArrayList<>();
+                    List<library> libraries = new ArrayList<>();
+                    String libraryName = null;
+
+                    String[] tmp2 = file.getFileName().toString().split(" ");
+
+                    String line = "";
+
+                    FileInputStream fis = new FileInputStream("C:\\Users\\user\\Downloads\\도서관 엑셀\\" +file.getFileName());
+
+                    XSSFWorkbook workbook = new XSSFWorkbook(fis);
+                    if (tmp2.length == 1) {
+                        libraryName = tmp2[0].split("\\.")[0];
+                    } else {
+                        StringBuilder sb = new StringBuilder();
+
+                        for (int i = 0; i < tmp2.length - 4; i++) {
+
+                            sb.append(tmp2[i]);
+
+                        }
+                        libraryName = sb.toString();
+                    }
 
 
+// 시트의 수
+                    int sheets = workbook.getNumberOfSheets();
+                    for (int i = 0; i < sheets; i++) {
+                        Sheet sheet = workbook.getSheetAt(i);
+
+
+	/* 1. row 얻기 : getPhysicalNumberOfRows();
+	int rows = sheet.getPhysicalNumberOfRows();
+	for(int r = 0; r < rows; r++){
+		..
+	}
+	*/
+
+                        // 2. row 얻기 : iterator();
+                        Iterator<Row> rowIterator = sheet.iterator();
+                        rowIterator.next();
+                        while (rowIterator.hasNext()) {
+                            Row row = rowIterator.next();
+
+
+                            List<String> aLine = new ArrayList<>();
+                            // 2. cell 얻기 : cellIterator();
+                            Iterator<Cell> cellIterator = row.cellIterator();
+                            while (cellIterator.hasNext()) {
+                                Cell cell = cellIterator.next();
+                                switch (cell.getCellType()) {
+                                    case BOOLEAN:
+
+                                        aLine.add(String.valueOf(cell.getBooleanCellValue()));
+                                        break;
+                                    case NUMERIC:
+                                        aLine.add(String.valueOf(cell.getNumericCellValue()));
+                                        break;
+                                    case STRING:
+                                        aLine.add(cell.getStringCellValue());
+                                        break;
+                                    case FORMULA:
+
+                                        break;
+                                }// switch
+                            }// while
+                            if (aLine.size() == 0) continue;
+
+                            libraries.add(new library(aLine.get(1), libraryName, aLine.get(3), aLine.get(4), aLine.get(2), aLine.get(9), aLine.get(5), aLine.get(8)));
+                        }// while
+                        libraryRepository.saveAll(libraries);
+                    }// for
+                }
+            }
             long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-            long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-            System.out.println("시간차이(m) : "+secDiffTime);
+            long secDiffTime = (afterTime - beforeTime) / 1000; //두 시간에 차 계산
+            System.out.println("시간차이(m) : " + secDiffTime);
 
         };
+
     }
 
 
--- src/main/java/com/example/demo/domain/library.java
+++ src/main/java/com/example/demo/domain/library.java
@@ -15,7 +15,7 @@ public class library {
     @SequenceGenerator(
             name = "BOARD_SEQ_GENERATOR",
             sequenceName = "BOARD_SEQ",
-            initialValue = 1, allocationSize = 50
+            initialValue = 1, allocationSize = 100
     )
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
             generator = "BOARD_SEQ_GENERATOR")
@@ -40,26 +40,15 @@ public class library {
     @Column(nullable = false)
     private String libraryName;
 
-    @Column(nullable = false,length = 2000)
-    private String fullText;
+
     public library(String bookName,String libraryName,String publisher,String publicationYear,String authors,String classNum,String isbn13,String vol) {
-        this.bookName =bookName;
+        this.bookName = bookName;
         this.libraryName = libraryName;
         this.publisher = publisher;
-        this.publicationYear =publicationYear;
+        this.publicationYear = publicationYear;
         this.authors = authors;
         this.classNum = classNum;
         this.isbn13 = isbn13;
         this.vol = vol;
-        StringBuffer sb = new StringBuffer();
-        sb.append(bookName);
-        sb.append(vol);
-        sb.append(" ");
-        sb.append(libraryName);
-        sb.append(" ");
-        sb.append(publisher);
-        sb.append(" ");
-        sb.append(authors);
-        this.fullText =sb.toString();
     }
 }
